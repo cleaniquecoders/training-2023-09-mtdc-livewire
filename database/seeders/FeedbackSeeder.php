aba031979bd8f9eae4b310355ec9c9b26e722c42
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-03 11:33:31 +0800

    Setup model, migration, seeder and factory

---
+++ database/seeders/FeedbackSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Feedback;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FeedbackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(! app()->isProduction()) {
+            Feedback::factory(rand(100, 500))->create();
+        }
+    }
+}
