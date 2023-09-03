8fd655bba2acddf694c7755cb34dc808e5db483f
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-03 14:34:43 +0800

    Created Livewire Component for Feedback form and datatable

---
+++ app/Livewire/Feedback/Datatable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Feedback;
+
+use Livewire\Component;
+
+class Datatable extends Component
+{
+    public function render()
+    {
+        return view('livewire.feedback.datatable');
+    }
+}
