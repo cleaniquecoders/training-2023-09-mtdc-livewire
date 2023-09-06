236980b21a02ce2fcac832de14116d32f98f6dc5
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-06 12:48:39 +0800

    Added modal

--- resources/views/alpinejs.blade.php
+++ resources/views/alpinejs.blade.php
@@ -2,6 +2,56 @@
     <div class="min-h-screen">
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+
+
+                <h1 class="text-3xl mb-4">Modal</h1>
+
+                <div x-data="{ open: false }" class="flex justify-center">
+                    <!-- Trigger -->
+                    <span x-on:click="open = true">
+                        <button type="button" class="rounded-md bg-white px-5 py-2.5 shadow">
+                            Open dialog
+                        </button>
+                    </span>
+
+                    <!-- Modal -->
+                    <div x-show="open" style="display: none" x-on:keydown.escape.prevent.stop="open = false"
+                        role="dialog" aria-modal="true" x-id="['modal-title']" :aria-labelledby="$id('modal-title')"
+                        class="fixed inset-0 z-10 overflow-y-auto">
+                        <!-- Overlay -->
+                        <div x-show="open" x-transition.opacity class="fixed inset-0 bg-black bg-opacity-50"></div>
+
+                        <!-- Panel -->
+                        <div x-show="open" x-transition x-on:click="open = false"
+                            class="relative flex min-h-screen items-center justify-center p-4">
+                            <div x-on:click.stop x-trap.noscroll.inert="open"
+                                class="relative w-full max-w-2xl overflow-y-auto rounded-xl bg-white p-12 shadow-lg">
+                                <!-- Title -->
+                                <h2 class="text-3xl font-bold" :id="$id('modal-title')">Confirm</h2>
+
+                                <!-- Content -->
+                                <p class="mt-2 text-gray-600">Are you sure you want to learn how to create an awesome
+                                    modal?</p>
+
+                                <!-- Buttons -->
+                                <div class="mt-8 flex space-x-2">
+                                    <button type="button" x-on:click="open = false"
+                                        class="rounded-md border border-gray-200 bg-white px-5 py-2.5">
+                                        Confirm
+                                    </button>
+
+                                    <button type="button" x-on:click="open = false"
+                                        class="rounded-md border border-gray-200 bg-white px-5 py-2.5">
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <hr class="my-4">
+
                 <h1 class="text-3xl mb-4">Counter</h1>
                 <div x-data="{ count: 0 }" class="flex items-center">
 
@@ -35,8 +85,8 @@
                 @php
                     $data = [];
 
-                    for ($i=0; $i < rand(25, 100); $i++) {
-                        $data[] = rand(1,250).'.'.rand(1,250).'.'.rand(1,250).'.'.rand(1,250);
+                    for ($i = 0; $i < rand(25, 100); $i++) {
+                        $data[] = rand(1, 250) . '.' . rand(1, 250) . '.' . rand(1, 250) . '.' . rand(1, 250);
                     }
                 @endphp
 
@@ -55,8 +105,8 @@
 
                     <ul class="my-4">
                         <template x-for="item in filteredItems" :key="item">
-                            <li class="my-2" >
-                                <x-input type="checkbox"  />
+                            <li class="my-2">
+                                <x-input type="checkbox" />
                                 <span x-text="item"></span>
                             </li>
                         </template>
