76c8942cde958037faf2fea720226d06b864f812
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2023-09-06 12:56:15 +0800

    Add Tooltip

--- resources/views/alpinejs.blade.php
+++ resources/views/alpinejs.blade.php
@@ -3,6 +3,61 @@
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
 
+                <h1 class="text-3xl mb-4">
+                    Tooltip
+                </h1>
+                <div>
+
+                    <!-- Tippy.js -->
+                    <!-- https://atomiks.github.io/tippyjs/v6 -->
+                    <script src="https://unpkg.com/@popperjs/core@2"></script>
+                    <script src="https://unpkg.com/tippy.js@6"></script>
+
+                    <!-- Usage -->
+                    <div class="flex items-center justify-center gap-2">
+                        <button x-data x-tooltip="I am a tooltip!" type="button"
+                            class="rounded-md bg-white px-5 py-2.5 shadow">
+                            Hover over me
+                        </button>
+
+                        <button x-data @click="$tooltip('I am a tooltip!')" type="button"
+                            class="rounded-md bg-white px-5 py-2.5 shadow">
+                            Click me
+                        </button>
+                    </div>
+
+                    <!-- Source -->
+                    <script>
+                        document.addEventListener('alpine:init', () => {
+                            // Magic: $tooltip
+                            Alpine.magic('tooltip', el => message => {
+                                let instance = tippy(el, {
+                                    content: message,
+                                    trigger: 'manual'
+                                })
+
+                                instance.show()
+
+                                setTimeout(() => {
+                                    instance.hide()
+
+                                    setTimeout(() => instance.destroy(), 150)
+                                }, 2000)
+                            })
+
+                            // Directive: x-tooltip
+                            Alpine.directive('tooltip', (el, {
+                                expression
+                            }) => {
+                                tippy(el, {
+                                    content: expression
+                                })
+                            })
+                        })
+                    </script>
+                </div>
+                <hr class="my-4">
+
                 <h1 class="text-3xl mb-4">Toggle</h1>
                 <div>
 
